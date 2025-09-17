1758941dea57216be540ab5f7bdd66973f26a030
junaidbinjaman <me@junaidbinjaman.com>
2025-09-17 18:10:19 +0600

    init: I'm about to start nextjs practice agian for a new client project. So this is an initial commit

--- src/plugins/openVariant.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-const plugin = require('tailwindcss/plugin');
-
-const openVariant = plugin(function({ addVariant }: {addVariant: (name: string, definition: string) => void}) {
-    addVariant('group-open', ":merge(.group).open &");
-});
-
-module.exports = openVariant;
